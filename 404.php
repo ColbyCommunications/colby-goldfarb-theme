f4f4de88866f1bafbce05432f28a3aa71efba447
Brandon K. Waltz <bkwaltz@colby.edu>
2019-10-27 19:31:59 -0400

    adds 404 page template

---
+++ 404.php
@@ -0,0 +1,12 @@
+<div id="main-wrapper" class="container">
+   <div class="container" role="main">
+        <div class="row">
+            <div class="article-container">
+                <header><h1 class="entry-title">404 Not Found</h1></header>
+                <div class="entry-content">
+                    <p>Not Found</p>     
+                </div>
+            </div><!--.article-container -->
+        </div><!--.row -->
+    </div><!-- .container role: main -->
+</div>
