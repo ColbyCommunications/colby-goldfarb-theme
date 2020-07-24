b2ec075d1ba15b62df5e6874d08f2a6a2c1fa742
Brandon K. Waltz <bkwaltz@colby.edu>
2020-07-24 11:54:00 -0400

    adds default templates

---
+++ resources/views/template-canvas.php
@@ -0,0 +1,9 @@
+{{--
+    Template Name: Canvas
+--}}
+
+@extends('layouts.canvas')
+
+@section('content')
+    @include('partials.content-page')
+@endsection
--- resources/views/template-no-footer.blade.php
+++ resources/views/template-no-footer.blade.php
@@ -1,12 +1,10 @@
 {{--
-  Template Name: Custom Template
+    Template Name: No Footer
 --}}
 
-@extends('layouts.app')
+@extends('layouts.no-footer')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
     @include('partials.content-page')
-  @endwhile
 @endsection
