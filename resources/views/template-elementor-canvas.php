7f35c80cced7e534f829f5c4829c2c74ea5f0d35
Brandon K. Waltz <bkwaltz@colby.edu>
2020-07-25 13:26:41 -0400

    make elementor canvas and no-footer the same

--- resources/views/template-elementor-canvas.php
+++ resources/views/template-elementor-canvas.php
@@ -2,10 +2,11 @@
     Template Name: Colby Base Theme Elementor Canvas
 --}}
 
-@extends('layouts.elementor-canvas')
+@extends('layouts.no-footer')
 
 @section('content')
     @while(have_posts()) @php the_post() @endphp
+        @include('partials.page-header')
         @include('partials.content-page')
     @endwhile
 @endsection
