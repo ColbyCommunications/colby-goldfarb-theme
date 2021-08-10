30fa15bcf6831d7d453b7e887f0680e003b8c742
Brandon K. Waltz <bwaltz6@gmail.com>
2021-08-10 16:28:22 -0400

    updates

--- resources/views/layouts/annual-report-2020.blade.php
+++ resources/views/layouts/annual-report-2020.blade.php
@@ -2,10 +2,8 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head-annual-report')
   <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
     @include('partials.header-annual-report')
-        @yield('content')
-    @php do_action('get_footer') @endphp
+    @yield('content')
     @include('partials.footer-annual-report')
     @php wp_footer() @endphp
   </body>
