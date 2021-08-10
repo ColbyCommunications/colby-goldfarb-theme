7e3db53ab6f8e33b7d57a84ae676f4c68cfa6b9c
Brandon K. Waltz <bwaltz6@gmail.com>
2021-08-10 15:16:52 -0400

    updates

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,11 +3,6 @@
   @include('partials.head')
   <body @php body_class() @endphp>
     @yield('fullbody')
-
-     @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
-
     <script src="{{ get_theme_file_uri() . '/libraries/fslightbox-basic-3.2.1/fslightbox.js' }}"></script>
   </body>
 </html>
