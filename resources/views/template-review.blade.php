c5cc2b3bffb62f0710456982622154a59880162a
Brandon K. Waltz <bwaltz6@gmail.com>
2021-08-18 09:26:40 -0400

    updates

--- resources/views/template-review.blade.php
+++ resources/views/template-review.blade.php
@@ -24,9 +24,6 @@
                     <div class="inner__shape">
                         <h1>{{ $data['annual_report_hero_title'] }}</h1>
                         {!! $data['annual_report_hero_text'] !!}
-                        @if ($data['annual_report_hero_text'])
-                            <a href="{{ $data['annual_report_hero_cta']['url'] }}"  {{ $data['annual_report_hero_cta']['target'] }} class="btn-underline"> {{ $data['annual_report_hero_cta']['text'] }}</a>
-                        @endif
                     </div>
                     <div class="hero-background-shape"></div>
                 </div>
